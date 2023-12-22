518ee38e6609406902bdd865c1f9d8b0802169de
Oh Ga Eun <dhrkdms4113@naver.com>
2023-12-22 22:51:37 +0900

    react native init

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    // <View style={styles.container}>
+    <View
+      // 이렇게도 가능
+      style={{
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Text style={styles.text}>Hello</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// StyleSheet.create 없어도 작동은 하지만 속성 자동완성이 안됨
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: "#fff",
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  // },
+  text: {
+    fontSize: 28,
   },
 });
